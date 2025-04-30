a07659f13216830412d2573896cbff7db4ea4938
showlyy <showlyweb@gmail.com>
2025-04-30 15:46:39 +0500

    Обновил сессию и токен для бота

--- get_session_fixed.py
+++ get_session_fixed.py
@@ -1,20 +1,23 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
-api_id = 26437413
-api_hash = "e1afb084d8c5896f0b685eeaa1b994f7"
+# Используем новый api_id и api_hash
+api_id = 26437413  # Здесь оставь без изменений
+api_hash = "e1afb084d8c5896f0b685eeaa1b994f7"  # Новый api_hash от твоего бота
 
+# Создание клиента для генерации сессии
 client = TelegramClient(
     StringSession(),
     api_id,
     api_hash,
-    device_model="Windows PC",
-    system_version="10.0",
-    app_version="9.4",
-    lang_code="en",
+    device_model="Windows PC",  # Маскировка устройства
+    system_version="10.0",  # Маскировка системы
+    app_version="9.4",  # Версия приложения
+    lang_code="en",  # Язык
     system_lang_code="en"
 )
 
+# Генерация сессии
 with client:
     print("Войдите в Telegram...")
     session = client.session.save()
